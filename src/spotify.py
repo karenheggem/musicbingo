0f52da0531e97d0817ff7ab8a205116de1cb32d9
Karen Heggem <karen-heggem@hotmail.com>
2020-02-08 19:13:26 +0100

    wip

--- src/spotify.py
+++ src/spotify.py
@@ -4,7 +4,7 @@ import spotipy.util as util
 def login(username: str = None,
           scope='user-library-read',
           client_id='8dc4d83d8d5646a2b4838a873d23a6ec',
-          client_secret='447f394bc3374e5dbbc2153f0d1a81a8',
+          client_secret='8c0cbcbcaaac47dd8044e332a9c7393c',
           redirect_uri='http://localhost/'):
 
     if username is None:
